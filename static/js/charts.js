93c0909027b57cc7b461e8fdeddab3bbf831867c
Bettinadavis11 <86321353+Bettinadavis11@users.noreply.github.com>
2021-09-27 22:07:47 -0700

    Final draft

--- static/js/charts.js
+++ static/js/charts.js
@@ -51,8 +51,6 @@ function buildMetadata(sample) {
 
   });
 }
-
-// Deliverable 1
 // 1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
@@ -95,7 +93,7 @@ function buildCharts(sample) {
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", barData, barLayout);
 
-    // Deliverable 2
+  
     // 1. Create the trace for the bubble chart.
     var bubbleData = [{
       x: otuIds,
@@ -105,7 +103,7 @@ function buildCharts(sample) {
       marker: {
         size: sampleValues,
         color: otuIds,
-        colorscale: 'Bluered'
+        colorscale: 'Earth'
       }
     }];
     // 2. Create the layout for the bubble chart.
@@ -123,8 +121,7 @@ function buildCharts(sample) {
     // 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
 
-    // Deliverable 3
-    // 1. Create a variable that filters the metadata array for the object with the desired sample number.
+      // 1. Create a variable that filters the metadata array for the object with the desired sample number.
     // 2. Create a variable that holds the first sample in the metadata array.
     var mdata = data.metadata.filter(sampleObj => sampleObj.id == sample)[0]
     // 3. Create a variable that holds the washing frequency.
@@ -140,16 +137,16 @@ function buildCharts(sample) {
         axis: { range: [null, 10], tickwidth: 2 },
         bar: { color: "maroon" },
         steps: [
-          { range: [0, 2], color: "AliceBlue" },
-          { range: [2, 4], color: "LightBlue" },
-          { range: [4, 6], color: "SkyBlue" },
-          { range: [6, 8], color: "RoyalBlue" },
-          { range: [8, 10], color: "Navy" },
+          { range: [0, 2], color: "orangered" },
+          { range: [2, 4], color: "lemonchiffon" },
+          { range: [4, 6], color: "aquamarine" },
+          { range: [6, 8], color: "mediumturquoise" },
+          { range: [8, 10], color: "steelblue" },
         ]}
     }];
     // 5. Create the layout for the gauge chart.
     var gaugeLayout = { 
-      title: "Belly Button Washing Frequency",
+      title: "Washing Frequency",
       titlefont: {"size": 22},
       width: 500, 
       height: 325, 
